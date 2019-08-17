f49273354acdcc025bf40f0c3f73d228cc799ea6
Brett Neese <brett@neese.rocks>
2019-08-16 19:48:32 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+customElements.define(
+  "o-embed",
+  class extends HTMLElement {
+    constructor() {
+      super();
+      this.init();
+    }
+
+    async init() {
+      const divElem = document.createElement("div");
+      const url = this.getAttribute("ref");
+      divElem.textContent = url;
+
+      const shadowRoot = this.attachShadow({ mode: "open" });
+      this.embed(url, shadowRoot);
+    }
+
+    async embed(url, shadowRoot) {
+      let endpoint = await this.getOembedEndpoint(url);
+
+      let response = await fetch(`${endpoint}?url=${url}`);
+      let json = await response.json();
+      let html = await json.html;
+
+      shadowRoot.innerHTML = html;
+    }
+
+    async getEndpoints() {
+      let response = await fetch(
+        `https://raw.githubusercontent.com/brettneese/oembed-component/master/src/endpoints-regexp.json`
+      );
+      let json = await response.json();
+
+      return json;
+    }
+
+    // https://github.com/thangman22/oembed-component
+    async getOembedEndpoint(url) {
+      let endpoints = await this.getEndpoints();
+
+      console.log(endpoints);
+
+      let r = Object.keys(endpoints).filter(endpoint => {
+        let foundPattern = endpoints[endpoint].filter(pattern =>
+          new RegExp(pattern).test(url)
+        );
+        return foundPattern.length > 0;
+      })[0];
+
+      return r;
+    }
+  }
+);
